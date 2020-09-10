c944ff9373e0809f51519dccd66323cac11ddcb6
xandre9-git <jmjames673@gmail.com>
2020-09-09 23:11:28 -0400

    completed task 1

--- index.js
+++ index.js
@@ -28,20 +28,42 @@ Airplane.prototype.land = function () {
 
 /*
   TASK 1
-    - Write a Person Constructor that initializes `name` and `age` from arguments.
-    - All instances of Person should initialize with an empty `stomach` array.
+    - Write a Person Constructor that initializes `name` and `age` from arguments. x
+    - All instances of Person should initialize with an empty `stomach` array. x
     - Give instances of Person the ability to `.eat("someFood")`:
         + When eating an edible, it should be pushed into the `stomach`.
         + The `eat` method should have no effect if there are 10 items in the `stomach`.
-    - Give instances of Person the ability to `.poop()`:
+    - Give instances of Person the ability to `.poop()`: x
         + When an instance poops, its `stomach` should empty.
-    - Give instances of Person a method `.toString()`:
-        + It should return a string with `name` and `age`. Example: "Mary, 50"
+    - Give instances of Person a method `.toString()`: x
+        + It should return a string with `name` and `age`. Example: "Mary, 50" x
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 
 }
+
+Person.prototype.eat = function (str) {
+  if (this.stomach.length < 10)
+  this.stomach.push(str)
+  return this.stomach
+}
+
+// console.log(Person.eat("shit"))
+
+Person.prototype.poop = function () {
+  
+    this.stomach = []
+  
+}
+
+Person.prototype.toString = function () {
+  return `${this.name}, ${this.age}`
+}
+
 
 /*
   TASK 2
